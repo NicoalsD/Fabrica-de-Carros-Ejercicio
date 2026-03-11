57cf3cf3ede7a306aca27e0658485d2b2e00cdc7
NicoalsD <22334455nico@gmail.com>
2026-03-10 20:37:03 -0500

    Implementación del escenario propuesto en el ejercicio

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hola");
+    public static void main(String[] args) {
+        System.out.println("Bienvenido al sistema para crear carros");
+
+        Planta planta1 = new Planta(15.5f, TipoLlanta.ALTO_DESEMPEÑO, 1200f, MaterialChasis.ACERO, Colores.VERDE);
+
+        Planta planta2 = new Planta(16.0f, TipoLlanta.NIEVE, 1100f, MaterialChasis.ALUMINIO, Colores.ROJO);
+
+        System.out.println("// 100 CARROS DE LA PLANTA 1");
+        
+        for (int i = 1; i <= 2; i++) {
+            System.out.println("Carro n" + i + " " + planta1.fabricar());
+        }      
+
+        System.out.println("// 100 CARROS DE LA PLANTA 2");
+        for (int i = 1; i <= 2; i++) {
+                System.out.println("Carro n" + i + " " + planta2.fabricar());
+        }      
     }
 }
