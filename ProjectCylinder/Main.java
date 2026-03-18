5fc6fdae1d5dcb1802d1e6c8e97b79db236ac6dd
NicoalsD <22334455nico@gmail.com>
2026-03-18 16:32:13 -0500

    Add Main class to demonstrate Circle and Cylinder functionality

---
+++ ProjectCylinder/Main.java
@@ -0,0 +1,13 @@
+package ProjectCylinder;
+
+public class Main {
+    public static void main(String[] args) {
+        Circle mCircle = new Circle();
+
+        System.out.println(mCircle);
+
+        Cylinder miCilindro = new Cylinder(10, 20, "Cagada");
+
+        System.out.println(miCilindro);
+    }
+}
